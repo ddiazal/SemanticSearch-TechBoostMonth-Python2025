5696b8bb62ce373b42226ada6f5127df64eeafcb
ddiazal <ddiazalmeida31@gmail.com>
2025-04-28 17:24:59 -0400

    update ss_testing.py

--- ss_testing.py
+++ ss_testing.py
@@ -2,5 +2,20 @@ import os
 import pandas as pd
 from semantic_search import SemanticSearch
 
+# Get OpenAI secret key
+SECRET_KEY: str = os.getenv("OPI_OPENAI_API_KEY")
+print(SECRET_KEY)
+
 # Instantiate class
 sem_search = SemanticSearch()
+
+# Data
+products: str = "products.json"
+# Query
+query: str = "High definition TV"
+
+query_response = sem_search(
+    data_file=products, query=query, file_type="json"
+)
+
+print(query_response)
