8cfd9e09c1484aff8222b0ba2946c033950d75be
ddiazal <ddiazalmeida31@gmail.com>
2025-04-28 21:44:33 -0400

    create stapp.py

---
+++ stapp.py
@@ -0,0 +1,6 @@
+import requests
+import streamlit as st
+
+st.set_page_config(page_title='Simple App', page_icon='<UNK>', layout='centered')
+st.image("images/chopy_soup_logo.jpeg", use_container_width=True)
+st.header(""":blue[Chopy-Soup] Search App.""", divider="rainbow")
